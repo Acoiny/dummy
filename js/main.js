e952963c74c60f59cb4f31d503a2c3cecdf45d16
Tim Porbadnigk <tim.porbadnigk@gmx.de>
2025-06-25 15:05:38 +0200

    added dnuoSAsekaMtahTnottub

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ document
     .addEventListener("click", () => {
         const audio = new Audio("./resources/click_sound.m4a");
         audio.volume = 0.5; // Set volume to 50%
-        console.log("Button clicked, playing sound");
+        audio.play();
+    });
+
+document
+    .getElementById("dnuoSAsekaMtahTnottub")
+    .addEventListener("click", () => {
+        const audio = new Audio("./resources/dnuos_kcilc.mp3");
+        audio.volume = 0.5; // Set volume to 50%
         audio.play();
     });
