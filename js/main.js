8dd1df158c89456ca11cc413bf3f33c260ce3bd5
Tim Porbadnigk <tim.porbadnigk@gmx.de>
2025-07-08 17:46:44 +0200

    added nothing button?

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+let sound = "click_sound";
+
 document
     .getElementById("buttonThatMakesASound")
     .addEventListener("click", () => {
-        const audio = new Audio("./resources/click_sound.m4a");
+        const audio = new Audio(`./resources/${sound}.mp3`);
         audio.volume = 0.5; // Set volume to 50%
         audio.play();
     });
@@ -9,7 +11,19 @@ document
 document
     .getElementById("dnuoSAsekaMtahTnottub")
     .addEventListener("click", () => {
-        const audio = new Audio("./resources/dnuos_kcilc.mp3");
+        const reversedName = sound.split("").reverse().join("");
+        const audio = new Audio(`./resources/${reversedName}.mp3`);
         audio.volume = 0.5; // Set volume to 50%
         audio.play();
     });
+
+const nothingButton = document.getElementById("nothing-button");
+
+// first click changes the text
+nothingButton.onclick = () => {
+    nothingButton.innerHTML += "?";
+
+    nothingButton.onclick = () => {
+        sound = sound === "click_sound" ? "click2_sound" : "click_sound";
+    };
+};
